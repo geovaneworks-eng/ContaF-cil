15c6c3a4d73d83b13da8d32d05820c7dc39d15c5
[dyad] <geovane.works@gmail.com>
2025-10-02 23:16:33 -0300

    [dyad] Forçando a limpeza do cache - wrote 1 file(s)

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import './index.css'; // Movido para o topo para forçar a invalidação do cache
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from '@/App';
-import './index.css';
 import ErrorBoundary from '@/components/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
